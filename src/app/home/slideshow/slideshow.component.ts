d6cb881fc63051a61a2cf156c0b046fff3374a16
Richard Blewett <richardcblewett@gmail.com>
2022-01-31 17:26:59 -0500

    more attempts at getting data

--- src/app/home/slideshow/slideshow.component.ts
+++ src/app/home/slideshow/slideshow.component.ts
@@ -17,21 +17,21 @@ export class SlideshowComponent implements OnInit {
 
   constructor(private picturesService:PicturesService) { }
 
-  ngOnInit(): void {
-    this.imageInput = this.picturesService.getAllPictures();
+  //ngOnInit(): void {
+    //this.imageInput = this.picturesService.getAllPictures();
     // this.imageInput.forEach( (e:any) => {
-    //    console.log(e.toString())
+    //     console.log(e.toString())
     // })
-  }
-  // ngOnInit(): void {
-  //   this.searchPictures //
-  //     .pipe(debounceTime(1000),distinctUntilChanged())
-  //     .subscribe( () => {
-  //       this.picturesService.getAllPictures()
-  //         .subscribe( response => {
-  //           this.images = response;
-  //           console.log(this.images[0])
-  //         })
-  //     })
   // }
+   ngOnInit(): void {
+    this.searchPictures //
+      .pipe(debounceTime(1000),distinctUntilChanged())
+      .subscribe( () => {
+        this.picturesService.getAllPictures()
+          .subscribe( response => {
+            this.imageInput = response;
+            console.log(this.imageInput[0])
+          })
+      })
+  }
 }
