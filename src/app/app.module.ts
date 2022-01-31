2c19e057ccf5a9a78d850b7963454606abbb5c61
Richard Blewett <richardcblewett@gmail.com>
2022-01-31 12:45:48 -0500

    adding the home componant|adding subcomponants textblock and slideshow.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SlideshowComponent } from './home/slideshow/slideshow.component';
+import { TextblockComponent } from './home/textblock/textblock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SlideshowComponent,
+    TextblockComponent
   ],
   imports: [
     BrowserModule,
