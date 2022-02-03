afa9f292a3fbe30b8265c707328e2d8f12877b58
Richard Blewett <richardcblewett@gmail.com>
2022-02-02 21:25:22 -0500

    git the search button as good as it will be for now

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Search} from './search';
 import {HttpClient} from "@angular/common/http";
 import {SearchServiceService} from "../search-result/search-service.service";
@@ -6,22 +6,29 @@ import {SearchServiceService} from "../search-result/search-service.service";
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+
 })
 
 
 export class SearchComponent implements OnInit {
 
-  result:any;
-  model = new Search(0,0);
-  submitted = false;
+  result: any;
+  model = new Search(0, 0);
+  count: number = 0;
+  submitted:boolean = false;
+
+
+  resetSubmit() {
+    this.submitted = false;
+  }
 
   onSubmit() {
-    this.submitted = true;
-    console.log(this.model);
+    this.submitted= true;
   };
 
-  constructor(private http:HttpClient, private searchService:SearchServiceService) { }
+  constructor(private http: HttpClient, private searchService: SearchServiceService) {
+  }
 
   ngOnInit(): void {
   }
